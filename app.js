a26ef5012dc6b42071bebe938d4182ae4c4e7c5f
McCloud <p446193@bremer.com>
2020-11-22 14:25:24 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Its a test'));
+app.get('/', (req, res) => res.send('Hello World! I just created my first pipeline and also was able to auto deploy it! This is sooooo cool and fun.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
