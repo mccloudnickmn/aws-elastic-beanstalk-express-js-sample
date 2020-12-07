f33844f9d1be0686fd38b58458199db526440ff2
McCloud <p446193@bremer.com>
2020-12-07 15:30:53 -0600

    Nick testing another commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I just created my first pipeline and also was able to auto deploy it! This is sooooo cool and fun.'));
+app.get('/', (req, res) => res.send('Hello World! I just created my first pipeline and also was able to auto deploy it!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
